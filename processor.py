e2def4ef032d7c6b474785dfbf3c40f3b2d0e07b
Krzysztof Adamkiewicz <kadamkiewicz835@gmail.com>
2019-04-24 19:07:58 +0200

    Add image ingestion system

--- processor.py
+++ processor.py
@@ -1,1 +1,19 @@
- 
+import cv2
+import sys
+
+this = sys.modules[__name__] 
+
+#images buffer
+this.rawImages = []
+
+#load image to the memory
+def loadImage(filename):
+    image = {
+        'image': cv2.imread(filename),
+        'filename': filename
+        }
+    this.rawImages.append(image)
+
+#empty data processor buffers
+def empty():
+    this.rawImages = []
--- spongebob.py
+++ spongebob.py
@@ -1,5 +1,38 @@
 import processor
 import commandLine
 import dataExporter as exporter
+import glob
+import sys
 
 userInput = commandLine.parse()
+
+#get list of files in the specified directory
+#extensions - list of all extensions
+#directory - searched directory
+#returns: file list
+def getFileList(extensions, directory):
+    files = []
+    for extension in extensions:
+        files.extend(glob.glob(directory + "/*." + extension))
+        
+    return files
+    
+#if user entered directory instead of data then use glob to find all images in the directory
+if userInput['inputIsFile'] == False:
+    images = getFileList(["jpg", "png", "gif", "bmp"], userInput['dataInputPath'])
+    print(str(len(images)) + " images detected in '" + userInput['dataInputPath'] + "'")
+    
+    #if they are no images availale in the directory then exit
+    if len(images) == 0:
+        print("No images in the directory!")
+        sys.exit(0)
+    
+    #process all images in the direcotry
+    for imageFile in images:
+        print("Loading file " + imageFile + " ...")
+        processor.loadImage(imageFile)
+            
+else:#just load single image
+    print("Loading file " + userInput['dataInputPath'] + " ...")
+    processor.loadImage(userInput['dataInputPath'])
+    
